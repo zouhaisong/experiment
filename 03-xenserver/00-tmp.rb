ee98438131fb3b7e0244f6ce8b66c009eccdb821
Haisong Zou <zouhaisong@gmail.com>
2015-09-18 11:30:32 +0800

    xenserver via fog

--- 03-xenserver/00-tmp.rb
+++ 03-xenserver/00-tmp.rb
@@ -2,11 +2,12 @@
 require 'active_support/all'
 require "pp"
 require 'fileutils'
-require 'fog'
 require 'uuidtools'
 require 'open-uri'
-
 require 'dotenv'
+require 'fog/xenserver'
+
+
 Dotenv.load(".env", ".env.default", "~/.env")
 
 xs_host = ENV['XS_HOST']
@@ -21,7 +22,8 @@ compute = Fog::Compute.new({
 })
 
 puts "Connected to #{xs_host}."
-
-
+vm = compute.servers.get_by_uuid("1fa5c9bc-69e3-a787-3d04-f413216c2d54")
+one_G = 1.gigabytes.to_i.to_s
+vm.set_memory_limits(one_G,one_G,one_G,one_G)
 ################################################################################
 
